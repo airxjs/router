95823bee6b7bf8149a1cd7c59b2db58f1c7ff81c
Alain <yinxulai@hotmail.com>
2024-04-19 22:06:07 +0800

    pack bundler

--- source/router.ts
+++ source/router.ts
@@ -124,7 +124,7 @@ export function Router(props: RouterProps) {
     return null
   }
 
-  function handleHistoryUpdate(action: Action, location: Location) {
+  function handleHistoryUpdate(_action: Action, location: Location) {
     const path = createPath(location)
     const matchResult = matchRoute(path)
     if (matchResult == null) return currentElement.value = null
